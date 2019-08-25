be8665afe73c3025160d5fe46c44965a1fbce2cf
ramishhassanbaig <mrhb1993@hotmail.com>
2019-08-25 14:20:56 +0500

    commit all counter app

---
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.counter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView mUnit;
+    Button mPlus;
+    Button mMinus;
+    Button mReset;
+    int number;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    mUnit=findViewById(R.id.unit);
+    mMinus=findViewById(R.id.minus_button);
+    mPlus=findViewById(R.id.plus_button);
+    mReset=findViewById(R.id.reset_botton);
+
+    mPlus.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            number++;
+            mUnit.setText(String.valueOf(number));
+        }
+    });
+    mMinus.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (number < 0){
+                number = 0;
+                mUnit.setText(String.valueOf(number));
+            }
+            else if (number > 0){
+                number--;
+                mUnit.setText(String.valueOf(number));
+            }
+        }
+    });
+    mReset.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (number != 0) {
+                number = 0;
+                mUnit.setText(String.valueOf(number));
+            }
+        }
+    });
+
+    }
+}
